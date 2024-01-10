2e9fa68109a4dee4a64f800b030992641f3cf5f3
Nelis Boucké <nelis@archiwise.com>
2024-01-10 21:57:30 +0100

    Allow to add a task (only response code)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use rocket::http::Status;
+use rocket::response::status;
+use status::Created;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/tasks")]
+fn add_task() -> Status {
+    Status::Created
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, add_task])
 }
 
 #[cfg(test)]
 mod test {
-    use super::rocket;
+    use rocket::http::{ContentType, Status};
     use rocket::local::blocking::Client;
-    use rocket::http::Status;
+
+    use super::rocket;
 
     #[test]
     fn hello_world() {
@@ -23,5 +34,23 @@ mod test {
 
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
+
+    #[test]
+    fn test_add_task() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+
+        
+        let response = client
+            .post("/tasks/")
+            .header(ContentType::JSON)
+            .body(r##"
+            {
+                "title": "a title"
+            }
+            "##)
+            .dispatch();
+
+        assert_eq!(response.status(), Status::Created);
     }
 }
