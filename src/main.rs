9c2d564fcb143111a428e4c16b4bde7027a29a17
Nelis Boucké <nelis@archiwise.com>
2024-01-10 21:33:26 +0100

    Add test for hello world

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,19 @@ fn index() -> &'static str {
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
 }
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get("/").dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
+}
