5be0ede1eee47a0654f957623dc2be8901380b97
Saket Kulkarni <dev@cassk.nl>
2024-03-26 20:49:35 +0100

    Fix scope todo_controller functions

--- src/controllers/todo_controller.rs
+++ src/controllers/todo_controller.rs
@@ -11,7 +11,7 @@ use crate::controllers::todo_update::TodoUpdate;
 use crate::core::Todo;
 
 #[post("/tasks", data = "<todo>")]
-pub fn add_task(todo: Json<TodoCreate>, todos: &State<Mutex<HashMap<String, Todo>>>) -> TodoCreatedResponse {
+fn add_task(todo: Json<TodoCreate>, todos: &State<Mutex<HashMap<String, Todo>>>) -> TodoCreatedResponse {
     let mut todos_map = todos.lock().unwrap();
     let todo_index = todos_map.len() + 1;
     todos_map.insert(todo_index.to_string(), Todo {
@@ -25,12 +25,12 @@ pub fn add_task(todo: Json<TodoCreate>, todos: &State<Mutex<HashMap<String, Todo
 }
 
 #[put("/tasks/<_id>", data = "<_input>")]
-pub fn update_task(_id: String, _input: Json<TodoUpdate>) -> Status {
+fn update_task(_id: String, _input: Json<TodoUpdate>) -> Status {
     Status::Ok
 }
 
 #[patch("/tasks/<id>", data = "<input>")]
-pub fn patch_task(id: String, input: Json<TodoUpdate>, todos: &State<Mutex<HashMap<String, Todo>>>) -> Status {
+fn patch_task(id: String, input: Json<TodoUpdate>, todos: &State<Mutex<HashMap<String, Todo>>>) -> Status {
     let mut todos_map = todos.lock().unwrap();
 
     match todos_map.get_mut(&id.to_string()) {
@@ -46,14 +46,14 @@ pub fn patch_task(id: String, input: Json<TodoUpdate>, todos: &State<Mutex<HashM
 }
 
 #[get("/tasks/<id>")]
-pub fn get_task(id: &str, todos: &State<Mutex<HashMap<String, Todo>>>) -> Option<Json<Todo>> {
+fn get_task(id: &str, todos: &State<Mutex<HashMap<String, Todo>>>) -> Option<Json<Todo>> {
     let todos_map = todos.lock().unwrap();
     todos_map.get(&id.to_string())
         .map(|todo| Json(todo.clone()))
 }
 
 #[delete("/tasks/<id>")]
-pub fn delete_task(id: &str, todos: &State<Mutex<HashMap<String, Todo>>>) -> Status {
+fn delete_task(id: &str, todos: &State<Mutex<HashMap<String, Todo>>>) -> Status {
     let mut todos_map = todos.lock().unwrap();
     let remove_status = todos_map.remove(id);
     if remove_status == None {
